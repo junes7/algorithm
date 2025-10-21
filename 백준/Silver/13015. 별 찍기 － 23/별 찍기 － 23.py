3887d62c1ab8fead093fce787b4a18688065e40b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-22 07:25:27 +0900

    [Silver IV] Title: 별 찍기 - 23, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/13015. 별 찍기 － 23/별 찍기 － 23.py
@@ -0,0 +1,10 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N = int(input())
+print('*'*N + ' '*(2*N-3) + '*'*N)
+for i in range(N-2):
+    print(' '*(i+1) + '*' + ' '*(N-2) + '*' + ' '*((N-2-i)*2- 1) + '*' + ' '*(N-2) + '*')
+print(' '*(N-1) + '*' + ' '*(N-2) + '*' + ' '*(N-2) + '*')
+for i in range(N-3, -1, -1):
+    print(' '*(i+1) + '*' + ' '*(N-2) + '*' + ' '*((N-2-i)*2- 1) + '*' + ' '*(N-2) + '*')
+print('*'*N + ' '*(2*N-3) + '*'*N)
