608a0e6f0e009adbf6f5d6d8247f8235fec9dd02
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-25 07:13:48 +0900

    [Silver V] Title: 욕심쟁이 돼지, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/3060. 욕심쟁이 돼지/욕심쟁이 돼지.py
@@ -0,0 +1,10 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+for _ in range(int(input())):
+    n=int(input())
+    food=sum([*map(int,input().split())])
+    day=1
+    while n>=food:
+        day+=1
+        food*=4
+    print(day)
