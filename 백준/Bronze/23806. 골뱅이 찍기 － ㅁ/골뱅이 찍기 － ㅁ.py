c485afb909e11843a81b9a4d1b1955cbc819070e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-26 10:24:09 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N = int(input())
+for _ in range(N):
+    print('@'*(5*N))
+for _ in range(N*3):
+    print('@'*N + ' '*(N*3) + '@'*N)
+for _ in range(N):
+    print('@'*(5*N))
