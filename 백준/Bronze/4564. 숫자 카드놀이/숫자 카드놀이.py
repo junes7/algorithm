27ed791b429b462863d7b350ecdff2e9194719a4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-27 21:08:45 +0900

    [Bronze II] Title: 숫자 카드놀이, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/4564. 숫자 카드놀이/숫자 카드놀이.py
@@ -0,0 +1,13 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+while 1:
+    n = input()
+    if n == '0':
+        break
+    while len(n) > 1:
+        res = 1
+        print(n, end=' ')
+        for c in n:
+            res *= int(c)
+        n = str(res)
+    print(n)
