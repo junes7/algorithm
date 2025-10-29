1bd53af84938929100f912dd624079e15852497c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-29 20:17:40 +0900

    [Bronze I] Title: 선 그리기, Time: 152 ms, Memory: 110708 KB -BaekjoonHub

---
+++ 백준/Bronze/16396. 선 그리기/선 그리기.py
@@ -0,0 +1,9 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+lst = [0 for _ in range(10001)]
+n = int(input())
+for _ in range(n):
+    a, b = map(int, input().split())
+    for i in range(a,b):
+        lst[i]=1
+print(sum(lst))
