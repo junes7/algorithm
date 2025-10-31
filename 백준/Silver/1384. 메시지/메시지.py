024dcae8a5dd0e303bc152388d3442589946310c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-10-31 22:46:53 +0900

    [Silver V] Title: 메시지, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/1384. 메시지/메시지.py
@@ -0,0 +1,24 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+g=1
+while True:
+    n=int(input())
+    if n==0:
+        break
+    kids=[]
+    bad_words=[]
+    for i in range(n):
+        tmp=list(input().rstrip().split())
+        kids.append(tmp[0])
+        for j in range(1,n):
+            if tmp[j]=="N":
+                bad_words.append([(i-j)%n,i])
+
+    print("Group "+str(g))
+    if len(bad_words)==0:
+        print("Nobody was nasty")
+    else:
+        for i in bad_words:
+            print(kids[i[0]]+" was nasty about "+kids[i[1]])
+    print("")
+    g+=1
