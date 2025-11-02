57ee6810b9b2ea680ab65b45b66ef38d3f663c3c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-02 22:05:54 +0900

    [Bronze II] Title: 교수님 그림이 깨지는데요?, Time: 104 ms, Memory: 110760 KB -BaekjoonHub

---
+++ 백준/Bronze/20205. 교수님 그림이 깨지는데요？/교수님 그림이 깨지는데요？.py
@@ -0,0 +1,12 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n, k = map(int, input().split())
+list1 = []
+for i in range(n):
+    list1.append(list(map(int, input().split())))
+for i in range(n):
+    for l in range(k):
+        for j in range(n):
+            for m in range(k):
+                print(list1[i][j], end = ' ')
+        print()
