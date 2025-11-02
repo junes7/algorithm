8b5fc9d86ac0d9483398c6ba3ab604849948edc4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-02 22:03:32 +0900

    [Bronze III] Title: 문문문, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/17210. 문문문/문문문.py
@@ -0,0 +1,10 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N = int(input())
+a = int(input())
+if N <= 5:
+    for i in range(N-1):
+        a = int(not a)
+        print(a)
+else:
+    print("Love is open door")
