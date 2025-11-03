aaac47d9cb0d7891c7d393549b2b2ced05a1dc8d
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-03 20:37:03 +0900

    [Silver V] Title: 피터팬 프레임, Time: 80 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/3054. 피터팬 프레임/피터팬 프레임.py
@@ -0,0 +1,23 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+S = input()
+frame_list = [[] for _ in range(5)]
+for i in range(1, len(S) + 1):
+    C = S[i - 1]
+    E = '*' if i % 3 == 0 else '#'
+    frame = [
+        f'..{E}..',
+        f'.{E}.{E}.',
+        f'{E}.{C}.{E}',
+        f'.{E}.{E}.',
+        f'..{E}..'
+    ]
+    for j in range(5):
+        f = frame[j]
+        if E == '#' and i < len(S):
+            f = f[:-1]
+        if 0 < i - 1 and (i - 1) % 3 == 0:
+            f = f[1:]
+        frame_list[j].append(f)
+for row in frame_list:
+    print(''.join(row))
