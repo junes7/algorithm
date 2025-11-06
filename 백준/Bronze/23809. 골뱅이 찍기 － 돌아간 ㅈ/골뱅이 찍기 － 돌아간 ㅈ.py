a4c6f44e7a62b6a539c64d78f21f5db3248e91e5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-07 07:15:56 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.py
@@ -0,0 +1,13 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N = int(input())
+for i in range(N):
+    print("@" * N + " " * 3 * N + "@" * N)
+for i in range(N):
+    print("@" * N + " " * 2 * N + "@" * N)
+for i in range(N):
+    print("@" * 3 * N)
+for i in range(N):
+    print("@" * N + " " * 2 * N + "@" * N)
+for i in range(N):
+    print("@" * N + " " * 3 * N + "@" * N)
