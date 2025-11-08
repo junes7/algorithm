60a22a4749b8e34318b99c9a71d4eab06dc7eb5e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-08 23:36:30 +0900

    [Bronze III] Title: 젓가락 게임, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/25642. 젓가락 게임/젓가락 게임.py
@@ -0,0 +1,12 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+A, B = map(int, input().split())	# 용태(A), 유진(B)
+while 1 :
+    B += A
+    if B >= 5 :
+        print('yt')
+        break
+    A += B
+    if A >= 5 :
+        print('yj')
+        break
