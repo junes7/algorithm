64c57d43453edd75700743174527a2fd1d026f83
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-10 23:00:31 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 136 ms, Memory: 110712 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,11 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+i = 1
+for _ in range(int(input())):
+    N = int(input())
+    a,b = map(int, input().split())
+    for _ in range(N):
+        a,b = map(int, input().split())
+    print("Material Management "+str(i))
+    print("Classification ---- End!")
+    i += 1
