1e508d4dfefddc0cd29640ff91ac1d2767a9bc43
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-11 22:20:04 +0900

    [Bronze II] Title: 투표, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/10040. 투표/투표.py
@@ -0,0 +1,18 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+N, M = map(int, input().split())
+A = [int(input()) for _ in range(N)]
+B = [int(input()) for _ in range(M)]
+count = [0] * N
+for i in range(M) :
+    for j in range(N) :
+        if A[j] <= B[i] :
+            count[j] += 1
+            break
+max_vote = 0
+who = -1
+for i in range(N) :
+    if max_vote < count[i] :
+        max_vote = count[i]
+        who = i
+print(who + 1)
