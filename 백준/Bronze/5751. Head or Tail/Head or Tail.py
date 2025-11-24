181b38fcce4675d02a3d9bd4234e9e6da456ce7f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-11-25 01:38:21 +0900

    [Bronze III] Title: Head or Tail, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/5751. Head or Tail/Head or Tail.py
@@ -0,0 +1,14 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+while 1:
+    N = int(input())
+    if N == 0:
+        break
+    li = list(map(int, input().split()))
+    j = m = 0
+    for n in li:
+        if n == 1:
+            j += 1
+        else:
+            m += 1
+    print(f"Mary won {m} times and John won {j} times")
