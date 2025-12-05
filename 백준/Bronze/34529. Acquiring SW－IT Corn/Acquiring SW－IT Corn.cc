f424cf4f0e8cf47126745de41d7dd598ab889baa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-05 17:13:12 +0900

    [Bronze IV] Title: Acquiring SW-IT Corn, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34529. Acquiring SW－IT Corn/Acquiring SW－IT Corn.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void) {
+    vector<int> price(3),plan(3),unit={100,50,20};
+    int total=0;
+    for(int i=0;i<3;i++)
+        cin>>price[i];
+    for(int i=0;i<3;i++)
+        cin>>plan[i];
+    for(int i=0;i<3;i++) {
+        total+=plan[i]/unit[i]*price[i];
+    }
+    cout<<total;
+    return 0;
+}
