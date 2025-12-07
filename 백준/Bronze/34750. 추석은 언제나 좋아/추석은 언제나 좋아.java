0b863681fc080ff0a75d0ff3c612f945a7e3badd
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-07 17:31:21 +0900

    [Bronze IV] Title: 추석은 언제나 좋아, Time: 96 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Bronze/34750. 추석은 언제나 좋아/추석은 언제나 좋아.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        double don,gi;
+        if(n>=1000000) {
+            don=n*0.2;
+            gi=n*0.8;
+        } else if(n>=500000 && n<1000000) {
+            don=n*0.15;
+            gi=n*0.85;
+        } else if(n>=100000 && n<500000) {
+            don=n*0.1;
+            gi=n*0.9;
+        } else {
+            don=n*0.05;
+            gi=n*0.95;
+        }
+        System.out.print((int)don+" "+(int)gi);
+    }
+}
