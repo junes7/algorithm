90c600ff348d3e9ac903ba418f15cae02a15b2bb
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-07 22:46:24 +0900

    [Bronze V] Title: Hack-a-Holics, Time: 176 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/34443. Hack－a－Holics/Hack－a－Holics.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n=scan.nextInt();
+        int c=scan.nextInt();
+        int p=scan.nextInt();
+        System.out.println(n*p);
+    }
+}
