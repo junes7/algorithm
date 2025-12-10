1330c0a16e1207c4b827965086ce697062186a39
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-10 16:25:39 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 92 ms, Memory: 13020 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int k = scan.nextInt();
+        int c = scan.nextInt();
+        System.out.print(m*c);
+    }
+}
