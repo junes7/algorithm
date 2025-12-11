e5cb2db63e687e84bc370c7d5794bcdfae412c16
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-11 16:17:08 +0900

    [Bronze IV] Title: Left Beehind, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/24768. Left Beehind/Left Beehind.py
+++ 백준/Bronze/24768. Left Beehind/Left Beehind.py
@@ -3,12 +3,12 @@ input=lambda:sys.stdin.readline().rstrip()
 while 1:
     x,y=map(int,input().split())
     if x==0 and y==0: break
-    if x+y!=13:
+    if x+y==13:
+        print("Never speak again.")
+    else:
         if x>y:
             print("To the convention.")
         elif x<y:
             print("Left beehind.")
         else:
             print("Undecided.")
-    else:
-        print("Never speak again.")
