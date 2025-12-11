aa2a1b2b8d5bf57f1140804c9a25a306fd53f3fe
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-11 15:44:38 +0900

    [Bronze IV] Title: ГРАДИНА, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/24294. ГРАДИНА/ГРАДИНА.py
+++ 백준/Bronze/24294. ГРАДИНА/ГРАДИНА.py
@@ -1,4 +1,4 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 w1,h1,w2,h2=int(input()),int(input()),int(input()),int(input())
-print(4+2*max(w1,w2)+2*(h1+h2))
+print(4+max(w1,w2)*2+(h1+h2)*2)
