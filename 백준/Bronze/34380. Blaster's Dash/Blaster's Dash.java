82ce73b39eb11b6152eb056b5af47808af921df2
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-11 13:34:40 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 104 ms, Memory: 12840 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m=scan.nextInt();
+        int v=scan.nextInt();
+        int n=scan.nextInt();
+        int k=scan.nextInt();
+        System.out.println(2*(20+n));
+    }
+}
