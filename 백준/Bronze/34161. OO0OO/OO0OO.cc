4b3b3796641c7b21b35cfae8d15b088e62afeb6c
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-12 20:54:41 +0900

    [Bronze IV] Title: OO0OO, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    string st;
+    for(int i=0;i<3;i++)
+        cin>>st;
+    for(int i=0;i<10000;i++)
+        cout<<-1<<"\n";
+    return 0;
+}
