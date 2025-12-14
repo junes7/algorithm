f5612c7df0f6bf29cc06be2a939da5c72c1da00b
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-14 22:50:03 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(void) {
+    int n;
+    cin>>n;
+    cout<<fixed;
+    cout.precision(0);
+    cout<<pow(10,9);
+    return 0;
+}
