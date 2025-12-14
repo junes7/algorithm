1b391fd9c347add1c7d4cd0a753ba67426476c45
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-14 23:36:16 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 100 ms, Memory: 12884 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int rlt=(int)Math.pow(10, 9);
+        System.out.print(rlt);
+    }
+}
