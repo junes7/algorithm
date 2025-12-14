2bbb28bfd5951914c7321be2ccd03677892ee965
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-14 19:04:48 +0900

    [Bronze III] Title: 크림빵, Time: 100 ms, Memory: 109544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -1,16 +1,12 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 n,k,p=map(int,input().split())
-cnt,rlt=0,0
-arr=[*map(int,input().split())]
-for i in range(n*k):
-    if i>0 and i%k==0:
-        if cnt<p:
-            rlt+=1
-        cnt=0
-    else:
-        if arr[i]==0:
+arr,rlt=[*map(int,input().split())],0
+for i in range(n):
+    cnt=0
+    for j in range(k*i,k*(i+1)):
+        if arr[j]==0:
             cnt+=1
-if cnt<p:
-    rlt+=1
+    if cnt<p:
+        rlt+=1
 print(rlt)
