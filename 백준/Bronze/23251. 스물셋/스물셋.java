69cb128e868370c146113acab61a0e5eb0ee712e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-15 14:52:15 +0900

    [Bronze II] Title: 스물셋, Time: 2976 ms, Memory: 215668 KB -BaekjoonHub

---
+++ 백준/Bronze/23251. 스물셋/스물셋.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int k,t = scan.nextInt();
+        for(int i=0;i<t;i++) {
+            k=scan.nextInt();
+            System.out.println(23*k);
+        }
+    }
+}
