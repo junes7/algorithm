ffe5f963769f3594aa4d0c876b55edfa742e7e41
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-15 15:55:21 +0900

    [Bronze II] Title: 9진수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/14491. 9진수/9진수.py
+++ 백준/Bronze/14491. 9진수/9진수.py
@@ -3,8 +3,5 @@ input=lambda:sys.stdin.readline().rstrip()
 def solve(r,n):
     if n==0:
         return r
-    if n%9:
-        return solve(str(n%9)+r,n//9)
-    else:
-        return solve("0"+r,n//9)
+    return solve((str(n%9) if n%9 else "0")+r,n//9)
 print(solve("",int(input())))
