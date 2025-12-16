426b96465bfde9393bf127b4b16bcdd947a4bbf8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-16 17:38:09 +0900

    [Bronze II] Title: 방 배정하기, Time: 36 ms, Memory: 32412 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -0,0 +1,12 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+a,b,c,n=map(int,input().split())
+cnt=0
+for i in range(1,n//a+1):
+    for j in range(1,n//b+1):
+        for k in range(1,n//c+1):
+            if a*i+b*j+c*k==n:
+                cnt=1
+                print(cnt)
+                exit(0)
+print(cnt)
