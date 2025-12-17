67582edd417275d849e34bcb4585ba6d30f9229f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-17 12:41:43 +0900

    [Silver III] Title: 1로 만들기, Time: 460 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,11 @@
 import sys
 input=lambda:sys.stdin.readline().rstrip()
 x=int(input())
-dp=[0]*(x+1)
+d=[0]*(x+1)
 for i in range(2,x+1):
-    dp[i]=dp[i-1]+1
-    if i%2==0:
-        dp[i]=min(dp[i],dp[i//2]+1)
+    d[i]=d[i-1]+1
     if i%3==0:
-        dp[i]=min(dp[i],dp[i//3]+1)
-print(dp[x])
+        d[i]=min(d[i],d[i//3]+1)
+    if i%2==0:
+        d[i]=min(d[i],d[i//2]+1)
+print(d[x])
