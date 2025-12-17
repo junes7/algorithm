ed971fd85d36db8c42a39afb3dc73ee5b725e3f5
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-17 20:00:39 +0900

    [Silver II] Title: 연속합, Time: 12 ms, Memory: 1896 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.c
+++ 백준/Silver/1912. 연속합/연속합.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(void) {
-    int n, *arr, *dp, maxn;
-    scanf("%d", &n);
-    arr = (int*)malloc(sizeof(int) * n);
-    dp = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-    dp[0] = arr[0], maxn = arr[0];
-    for (int i = 1; i < n; i++) {
-        dp[i] = arr[i] > arr[i] + dp[i - 1] ? arr[i] : arr[i] + dp[i - 1];
-        if (maxn < dp[i]) maxn = dp[i];
+    int n,maxn,*num,*dp;
+    scanf("%d",&n);
+    num=(int*)malloc(sizeof(int)*n);
+    dp=(int*)malloc(sizeof(int)*(n+1));
+    for(int i=0;i<n;i++)
+        scanf("%d",&num[i]);
+    dp[0]=num[0];
+    maxn=dp[0];
+    for(int i=1;i<n;i++) {
+        dp[i]=num[i]>(num[i]+dp[i-1])?num[i]:(num[i]+dp[i-1]);
+        if(maxn<dp[i]) maxn=dp[i];
     }
-    printf("%d", maxn);
+    printf("%d",maxn);
     return 0;
 }
