9e5df0ab252d5e29f9916b62b45eb6dc1f9054b4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-17 17:46:25 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.c
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int fibo(int n) {
-    int *f = (int *)malloc(sizeof(int) * (n + 1));
-    f[1] = 1, f[2] = 1;
-    for (int i = 3; i < n + 1; i++) {
-        f[i] = f[i - 1] + f[i - 2];
-    }
-    return f[n];
-}
-int fibonacci(int n) {
-    return n - 2;
-}
 int main(void) {
-    int n;
-    scanf("%d", &n);
-    printf("%d %d", fibo(n), fibonacci(n));
+    int n,*fibo;
+    scanf("%d",&n);
+    fibo=(int*)malloc(sizeof(int)*(n+1));
+    fibo[1]=fibo[2]=1;
+    for(int i=3;i<n+1;i++)
+        fibo[i]=fibo[i-1]+fibo[i-2];
+    printf("%d %d",fibo[n],n-2);
     return 0;
 }
