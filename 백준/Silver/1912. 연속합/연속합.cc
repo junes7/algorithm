6923c49784c6d136da4abb3f3a9b21f463decfac
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-17 18:47:16 +0900

    [Silver II] Title: 연속합, Time: 24 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,16 +1,17 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main(void) {
     int n;
-    cin >> n;
-    vector<int> arr(n), dp(n, 0);
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    dp[0] = arr[0];
-    for (int i = 1; i < n; i++)
-        dp[i] = max(arr[i], arr[i] + dp[i - 1]);
-    cout << *max_element(dp.begin(), dp.end());
+    cin>>n;
+    vector<int> num(n),dp(n+1,-1000);
+    for(int i=0;i<n;i++)
+        cin>>num[i];
+    dp[0]=num[0];
+    for(int i=1;i<n;i++) {
+        dp[i]=max(num[i],num[i]+dp[i-1]);
+    }
+    cout<<*max_element(dp.begin(),dp.end());
     return 0;
 }
