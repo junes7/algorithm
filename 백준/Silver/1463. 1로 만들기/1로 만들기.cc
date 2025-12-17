c148bf48f88e804ec5f056a319b8c09a76483374
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-17 13:49:16 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 int main(void) {
     int x;
-    cin >> x;
-    vector<int> dp(x + 1, 0);
-    for (int i = 2; i < x + 1; i++) {
-        dp[i] = dp[i - 1] + 1;
-        if (i % 3 == 0)
-            dp[i] = min(dp[i], dp[i / 3] + 1);
-        if (i % 2 == 0)
-            dp[i] = min(dp[i], dp[i / 2] + 1);
+    cin>>x;
+    vector<int> dp(x+1,0);
+    for(int i=2;i<x+1;i++) {
+        dp[i]=dp[i-1]+1;
+        if(i%3==0)
+            dp[i]=min(dp[i],dp[i/3]+1);
+        if(i%2==0)
+            dp[i]=min(dp[i],dp[i/2]+1);
     }
-    cout << dp[x];
+    cout<<dp[x];
     return 0;
 }
