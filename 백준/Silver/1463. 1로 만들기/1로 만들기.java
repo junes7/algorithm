f681aa7c189e57c664843a78daab6d85effd4934
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-17 14:07:14 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 16896 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int dp[] = new int[x+1];
+        for(int i=2;i<x+1;i++) {
+            dp[i]=dp[i-1]+1;
+            if(i%3==0)
+                dp[i]=Math.min(dp[i],dp[i/3]+1);
+            if(i%2==0)
+                dp[i]=Math.min(dp[i],dp[i/2]+1);
+        }
+        System.out.print(dp[x]);
+    }
+}
