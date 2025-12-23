664abe03c3093674c74b295a7a9bb029adb52566
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-23 16:00:35 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+    int n;
+    scanf("%d",&n);
+    char* st=(char*)malloc(sizeof(char)*(5*n+1));
+    for(int i=0;i<5;i++) {
+        for(int j=0;j<n;j++) {
+            memset(st,0,sizeof(char)*(n*5+1));
+            if(i<4 && i%2==0) {
+                for(int k=0;k<5*n;k++)
+                    strcat(st,"@");
+            } else {
+                for(int k=0;k<n;k++)
+                    strcat(st,"@");
+            }
+            strcat(st,"\n");
+            printf("%s",st);
+        }
+    }
+    return 0;
+}
