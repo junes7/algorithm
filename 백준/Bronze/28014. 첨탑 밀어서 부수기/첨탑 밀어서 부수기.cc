6f6375a75e72ff28bd0eb8f8afdbca039eee3f53
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-26 12:21:08 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 424 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.cc
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.cc
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int main(void) {
-    int n,rlt=1;
+    int n,prev,cur,rlt=1;
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n;
-    vector<int> arr(n);
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    for(int i=0;i<n-1;i++)
-        if(arr[i]<=arr[i+1])
-            rlt+=1;
+    cin>>prev;
+    for(int i=0;i<n-1;i++) {
+        cin>>cur;
+        if(prev<=cur) rlt+=1;
+        prev=cur;
+    }
     cout<<rlt;
     return 0;
 }
