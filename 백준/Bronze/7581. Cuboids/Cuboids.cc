339c0c61c9110a122b0f45949f81b41655c0134f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-26 15:22:45 +0900

    [Bronze III] Title: Cuboids, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7581. Cuboids/Cuboids.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int l,w,h,v;
+    while(1) {
+        cin>>l>>w>>h>>v;
+        if(l==0 && w==0 && h==0 && v==0) break;
+        if(l==0)
+            l=v/(w*h);
+        else if(w==0)
+            w=v/(l*h);
+        else if(h==0)
+            h=v/(l*w);
+        else
+            v=l*w*h;
+        cout<<l<<' '<<w<<' '<<h<<' '<<v<<"\n";
+    }
+    return 0;
+}
