a1fdde605422d6b45399b57a4dbf4b4a1a87aba4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-26 12:34:13 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 764 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.c
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 int main(void) {
-    int n,rlt=1;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
-    scanf("%d",&arr[i]);
-    for(int i=0;i<n-1;i++)
-        if(arr[i]<=arr[i+1])
-            rlt+=1;
+    int n,prev,cur,rlt=1;
+    scanf("%d %d",&n,&prev);
+    for(int i=1;i<n;i++) {
+        scanf("%d",&cur);
+        if(prev<=cur) rlt+=1;
+        prev=cur;
+    }
     printf("%d",rlt);
     return 0;
 }
