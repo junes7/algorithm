42d1bae20a20eb6f749b46f2decc11f2c5826354
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-26 12:49:18 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 804 ms, Memory: 330140 KB -BaekjoonHub

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
@@ -4,12 +4,14 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int rlt=1;
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int rlt=1,cur,prev = Integer.parseInt(st.nextToken());
+        int cur,prev =Integer.parseInt(st.nextToken());
         for(int i=1;i<n;i++) {
             cur = Integer.parseInt(st.nextToken());
-            if(prev<=cur) rlt+=1;
+            if(prev<=cur)
+                rlt+=1;
             prev=cur;
         }
         System.out.print(rlt);
