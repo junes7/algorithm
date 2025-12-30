a12815cbf8420f583525b5c26c6efff159d35463
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-30 18:36:11 +0900

    [Bronze III] Title: Ship Selection, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10180. Ship Selection/Ship Selection.c
+++ 백준/Bronze/10180. Ship Selection/Ship Selection.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(void) {
-    int T, N, D, v, f, c, cnt;
-    scanf("%d", &T);
-    for (int i = 0; i < T; i++) {
-        scanf("%d %d", &N, &D);
-        cnt = 0;
-        for (int j = 0; j < N; j++) {
-            scanf("%d %d %d", &v, &f, &c);
-            if (v * f / c >= D)
-                cnt += 1;
+    int t,n,d,v,f,c,cnt;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++) {
+        scanf("%d %d",&n,&d);
+        cnt=0;
+        for(int j=0;j<n;j++) {
+            scanf("%d %d %d",&v,&f,&c);
+            if(v*f/c>=d)
+                cnt+=1;
         }
-        printf("%d\n", cnt);
+        printf("%d\n",cnt);
     }
     return 0;
 }
