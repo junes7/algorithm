51648dd58fae08edf130765704bba18ff966364a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-30 13:02:36 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cin>>n;
+    string st="AKA";
+    for(int i=0;i<n;i++)
+        st+="RAKA";
+    cout<<st;
+    return 0;
+}
