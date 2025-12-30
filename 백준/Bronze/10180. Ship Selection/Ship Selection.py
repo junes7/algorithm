06009da044ece93478f336c6f335aad7e7eae225
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2025-12-30 18:32:20 +0900

    [Bronze III] Title: Ship Selection, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10180. Ship Selection/Ship Selection.py
+++ 백준/Bronze/10180. Ship Selection/Ship Selection.py
@@ -1,11 +1,10 @@
 import sys
-input=lambda:sys.stdin.readline().rstrip()
-T=int(input())
-for _ in range(T):
-    N,D=map(int,input().split())
+input=lambda:sys.stdin.readline().rstrip("\n")
+for _ in range(int(input())):
+    n,d=map(int,input().split())
     cnt=0
-    for _ in range(N):
-        vi,fi,ci=map(int,input().split())
-        if vi*fi/ci>=D:
+    for _ in range(n):
+        v,f,c=map(int,input().split())
+        if v*f/c>=d:
             cnt+=1
     print(cnt)
