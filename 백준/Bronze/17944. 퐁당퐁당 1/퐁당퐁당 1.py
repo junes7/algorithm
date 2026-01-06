5dbf8b91c403ab1baed706b9430823984f6a93f9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-01-06 22:48:15 +0900

    [Bronze III] Title: 퐁당퐁당 1, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/17944. 퐁당퐁당 1/퐁당퐁당 1.py
@@ -0,0 +1,11 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n,t = map(int,input().split())
+cnt,x = 0,1
+for i in range(t):
+    cnt +=x
+    if(cnt==2*n):
+        x = -1
+    if(cnt==1):
+        x = 1
+print(cnt)
