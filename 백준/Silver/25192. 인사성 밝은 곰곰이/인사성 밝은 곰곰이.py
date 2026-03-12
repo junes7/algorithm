3138f93e2b1759beb723ffa5d0f2389c800a25d8
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-03-12 22:38:32 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 164 ms, Memory: 117784 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -0,0 +1,12 @@
+n = int(input())
+gom = set()
+cnt = 0
+for _ in range(n):
+    user = input().strip()
+    if user == 'ENTER':
+        cnt += len(gom)
+        gom = set()
+    else:
+        gom.add(user)
+cnt += len(gom)
+print(cnt)
