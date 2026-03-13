d12271ad5d48f5cae9c43fe307f7e9b12642569a
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-03-13 22:43:15 +0900

    [Silver III] Title: Four Squares, Time: 116 ms, Memory: 113340 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,18 +1,9 @@
 import math
-def fourSquares(n):
-    # √n이 정수일 때
-    if int(math.sqrt(n)) == math.sqrt(n):
-        return 1
-    # √(n - i^2)이 정수일 때
-    for i in range(1, int(math.sqrt(n)) + 1):
-        if int(math.sqrt(n - i**2)) == math.sqrt(n - i**2):
-            return 2
-    # √(n - i^2 - j^2)이 정수일 때
-    for i in range(1, int(math.sqrt(n)) + 1):
-        for j in range(1, int(math.sqrt(n - i**2)) + 1):
-            if int(math.sqrt(n - i**2 - j**2)) == math.sqrt(n - i**2 - j**2):
-                return 3
-    # 남은 경우는 4
-    return 4
 n = int(input())
-print(fourSquares(n))
+dp = [0,1]
+for i in range(2, n+1):
+    minValue = 1e9
+    for j in range(1, int(math.sqrt(i)) + 1):
+        minValue = min(minValue, dp[i - j**2])
+    dp.append(minValue + 1)
+print(dp[n])
