7e6b0460d58ab0ca7c35f9b56c0cc7bd88a3cd1f
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-03-15 23:55:01 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.py
@@ -0,0 +1,8 @@
+import sys
+input=lambda:sys.stdin.readline().rstrip("\n")
+a1,a0=map(int,input().split())
+c,n0=int(input()),int(input())
+if (a1*n0 + a0<=c*n0) and a1<=c:
+    print(1)
+else:
+    print(0)
