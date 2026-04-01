39e8ad2636476c3b5f6748326170a7f1f53ff5a9
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-04-01 13:06:31 +0900

    [Silver V] Title: 타일 장식물, Time: 68 ms, Memory: 11516 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long a=1,b=1,t;
+        for(int i=0;i<n-1;i++) {
+            t=a;
+            a=b;
+            b=b+t;
+        }
+        System.out.println((a+b)*2);
+    }
+}
