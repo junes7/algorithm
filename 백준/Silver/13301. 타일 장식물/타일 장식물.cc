1a9eaf202bf8aab4a21aea46c19e7ec8a735e935
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-04-01 12:50:19 +0900

    [Silver V] Title: 타일 장식물, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    long long a = 1, b = 1, t;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++) {
+        t = a;
+        a = b;
+        b += t;
+    }
+    cout << (a + b) * 2;
+    return 0;
+}
