5cd365f3a60d7351ced88fc431f06e8b841951e4
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-05-14 11:09:02 +0900

    [level 3] Title: 네트워크, Time: 1.08 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,16 +1,17 @@
+from collections import deque
 def solution(n, computers):
-    def dfs(i):
-        stack = [i] 
-        while stack:
-            i = stack.pop()
-            visited[i] = True
+    def bfs(i):
+        queue = deque([i])
+        while queue:
+            node = queue.popleft()
+            visited[node] = True
             for j in range(n):
-                if not visited[j] and computers[j][i]:
-                    stack.append(j)
+                if not visited[j] and computers[j][node]:
+                    queue.append(j)
     visited = [False] * n
     cnt = 0
     for i in range(n):
         if not visited[i]:
+            bfs(i)
             cnt += 1
-            dfs(i)
     return cnt
