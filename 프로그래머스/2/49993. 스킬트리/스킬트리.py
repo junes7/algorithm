9c1ca8ed7e511107a32792e6aff37ca7fd6584fa
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-05-19 08:32:24 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,10 @@
+def solution(skill, skill_trees):
+    cnt = 0
+    for skill_tree in skill_trees:
+        s = ""                      # 하나의 스킬트리를 뽑을 때마다 s 초기화
+        for ch in skill_tree:       
+            if ch in skill:         # 스킬트리 중에 skill이 있다면 s에 추가
+                s += ch
+        if skill[:len(s)] == s:     # 만든 s를 기준으로 skill과 같다면 cnt += 1
+            cnt += 1
+    return cnt
