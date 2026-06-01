009bf8ade8c9423c3296c43fbf4f0aa60c7159e3
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-06-02 00:47:48 +0900

    [level 2] Title: 과제 진행하기, Time: 0.90 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -0,0 +1,33 @@
+def solution(plans):
+    rlt = []
+    for i in range(len(plans)):
+        h, m = map(int, plans[i][1].split(':'))
+        st = h*60+m
+        plans[i][1] = st
+        plans[i][2] = int(plans[i][2])
+    plans.sort(key=lambda x:x[1])
+    stack = []
+    for i in range(len(plans)):
+        if i == len(plans)-1:
+            stack.append(plans[i])
+            break
+        sub, st, t = plans[i]
+        nsub, nst, nt = plans[i+1]
+        if st + t <= nst:
+            rlt.append(sub)
+            temp_time = nst - (st+t)
+            while temp_time != 0 and stack:
+                tsub, tst, tt = stack.pop()
+                if temp_time >= tt:
+                    rlt.append(tsub)
+                    temp_time -= tt
+                else:
+                    stack.append([tsub, tst, tt - temp_time])
+                    temp_time = 0    
+        else:
+            plans[i][2] = t - (nst - st)
+            stack.append(plans[i])
+    while stack:
+        sub, st, tt = stack.pop()
+        rlt.append(sub)
+    return rlt
