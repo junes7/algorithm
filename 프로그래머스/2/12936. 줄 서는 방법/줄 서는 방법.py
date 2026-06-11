a9708dcdcf3378aa078f6699c0b1a661c09b6495
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-06-11 10:57:37 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,13 @@
+import math
+def solution(n, k):
+    rlt = []
+    people = [i for i in range(1, n+1)] 
+    for i in range(1, n+1):
+        divider = math.factorial(n-i)
+        idx = k // divider
+        if k % divider == 0:
+            idx -= 1
+        rlt.append(people[idx])
+        people.pop(idx)
+        k -= idx * divider
+    return rlt
