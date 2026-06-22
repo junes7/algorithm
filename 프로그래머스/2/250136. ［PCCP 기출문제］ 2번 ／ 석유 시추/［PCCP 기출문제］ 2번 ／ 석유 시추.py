64ee76bbeb6a36b9bf1f3892b9682d709c8cca6e
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-06-22 09:52:26 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 202.02 ms, Memory: 15.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -0,0 +1,36 @@
+from collections import deque
+def solution(land):
+    ans = 0
+    n = len(land)
+    m = len(land[0])
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    rlt = [0 for i in range(m+1)]
+    visited = [[0 for i in range(m)] for j in range(n)]
+    def bfs(a, b):
+        cnt = 0
+        visited[a][b] = 1
+        q = deque()
+        q.append((a,b))
+        min_y, max_y = b, b
+        while q:
+            x,y = q.popleft()
+            min_y = min(min_y, y)
+            max_y = max(max_y, y)
+            cnt += 1
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                    continue
+                if visited[nx][ny] == 0 and land[nx][ny] == 1:
+                    visited[nx][ny] = 1
+                    q.append((nx,ny))
+        for i in range(min_y, max_y+1):
+            rlt[i] += cnt
+    for i in range(n):
+        for j in range(m):
+            if visited[i][j] == 0 and land[i][j] == 1:
+                bfs(i,j)
+    ans = max(rlt)
+    return ans
