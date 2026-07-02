bc4641b652e0ab6f643fe97827be209c238cf354
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-07-02 10:09:17 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,1 +1,1 @@
-select ID,EMAIL,FIRST_NAME,LAST_NAME from DEVELOPER_INFOS where SKILL_1 = 'Python' or SKILL_2 = 'Python' or SKILL_3 = 'Python' order by ID;
+select id,email,first_name,last_name from developer_infos where skill_1='python' or skill_2='python' or skill_3='python' order by id;
