b123ffe9b72231d590f0341d393a8a51b5779570
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-09-14 19:42:18 +0900

    [level 3] Title: 인사고과, Time: 83.36 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -0,0 +1,14 @@
+def solution(scores):
+    rlt = 1
+    target = scores[0]
+    target_score = sum(scores[0])
+    scores.sort(key=lambda x: (-x[0], x[1]))
+    threshold = 0
+    for score in scores:
+        if target[0] < score[0] and target[1] < score[1]:
+            return -1
+        if threshold <= score[1]:
+            if target_score < score[0] + score[1]:
+                rlt += 1
+            threshold = score[1]
+    return rlt
