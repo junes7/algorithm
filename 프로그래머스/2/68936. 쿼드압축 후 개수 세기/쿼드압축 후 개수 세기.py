c1204c4e9161977a9ce125d98ef462db0fc3cf07
LEE JUN SUNG <58545479+junes7@users.noreply.github.com>
2026-09-24 00:37:08 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 72.76 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    rlt=[0,0]
+    length=len(arr)
+    def compression(a,b,l):
+        start=arr[a][b]
+        for i in range(a,a+l):
+            for j in range(b,b+l):
+                if arr[i][j]!=start:
+                    l=l//2
+                    compression(a,b,l)
+                    compression(a,b+l,l)
+                    compression(a+l,b,l)
+                    compression(a+l,b+l,l)
+                    return
+        rlt[start]+=1
+    compression(0,0,length)
+    return (rlt)
